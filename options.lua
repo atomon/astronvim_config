9692f8f89495d00c09c6f4407969d2f01cf0e81c
atomon <38009289+atomon@users.noreply.github.com>
2024-04-26 14:14:38 +0900

    feat: powershell

--- options.lua
+++ options.lua
@@ -1,16 +1,32 @@
+local opt_config = function()
+  local opt = {}
+
+  -- set to true or false etc.
+  opt["relativenumber"] = false -- sets vim.opt.relativenumber
+  opt["number"] = true -- sets vim.opt.number
+  opt["spell"] = false -- sets vim.opt.spell
+  opt["signcolumn"] = "auto" -- sets vim.opt.signcolumn to auto
+  opt["wrap"] = false -- sets vim.opt.wrap
+  opt["mousescroll"] = {"ver:9", "hor:6"}
+  opt["tabstop"] = 4
+  opt["shiftwidth"] = 4
+
+  if vim.fn.executable "pwsh" then
+    -- sets Nvim terminal for pwsh (https://www.siddharta.me/configuring-neovim-as-a-python-ide-2023.html)
+    opt["shell"] = vim.fn.executable "pwsh" and "pwsh" or "/bin/bash"
+    opt["shellcmdflag"] = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    opt["shellredir"] = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+    opt["shellpipe"] = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+    opt["shellquote"] = ""
+    opt["shellxquote"] = ""
+  end
+
+  return opt
+end
+
 -- set vim options here (vim.<first_key>.<second_key> = value)
 return {
-  opt = {
-    -- set to true or false etc.
-    relativenumber = false, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-    wrap = false, -- sets vim.opt.wrap
-    mousescroll = {"ver:9", "hor:6"},
-    tabstop = 4,
-    shiftwidth = 4,
-  },
+  opt = opt_config(),
   g = {
     mapleader = " ", -- sets vim.g.mapleader
     autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
